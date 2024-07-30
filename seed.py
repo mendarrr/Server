47e45ef6c2c87103e2b5f1d6061e58c16a142e42
ALLAN <allankipr6@gmail.com>
2024-07-30 20:51:20 +0300

    Seeded categories

--- seed.py
+++ seed.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             user = User(
                 username=fake.name(),
                 email=fake.email(),
-                password=fake.password(length=16),
+                password_hash=fake.password(length=16),
                 role="user"
             )
             users.append(user)
@@ -35,4 +35,14 @@ if __name__ == '__main__':
 
         print("Admins added successfully")
         
-        
+        categories=[
+            Category(category_name="Appetizers", image="https://i.pinimg.com/236x/74/62/97/746297f074bed5db7f26cee0c1decc28.jpg"),
+            Category(category_name="Starters", image="https://i.pinimg.com/236x/bf/82/05/bf82057cdd3c47275717dd04b17cc6b0.jpg"),
+            Category(category_name="Main Course", image="https://i.pinimg.com/236x/eb/6e/ec/eb6eecacd7a45143b9af85eb37468961.jpg"),
+            Category(category_name="Salads", image="https://i.pinimg.com/564x/f2/16/f0/f216f08940e8370d07f2295285a952df.jpg"),
+            Category(category_name="Desserts", image="https://i.pinimg.com/564x/9d/25/93/9d2593780fe22eba7acf1ea6e9e57110.jpg"),
+        ]
+        db.session.add_all(categories)
+        db.session.commit()
+
+        print("Categories added successfully")
